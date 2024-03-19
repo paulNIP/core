c3f0a65d06c5f3bd5a4fe53341050d82bb5b7afe
PaulOtim <paulalex.otim@outlook.com>
2024-03-20 02:12:07 +0300

    Upload function added

--- src/main/java/com/opolos/core/CoreApplication.java
+++ src/main/java/com/opolos/core/CoreApplication.java
@@ -1,13 +1,27 @@
 package com.opolos.core;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.opolos.core.security.services.FilesStorageService;
+
+import jakarta.annotation.Resource;
+
 @SpringBootApplication
-public class CoreApplication {
+public class CoreApplication implements CommandLineRunner {
+
+	@Resource
+    FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+  //    storageService.deleteAll();
+	  storageService.init();
 	}
 
 }
